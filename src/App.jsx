7175f5942b0c39d4c6b64e9eafdfcd3b1ff51fa4
Aslaag <lilice.flo@gmail.com>
2025-06-11 11:27:17 +0200

    build : Routes config

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,16 @@ import { BrowserRouter, Route, Routes } from "react-router";
 import { HomePage } from "./pages/HomePage";
 import { Movie } from "./pages/Movie";
 import { NotFound } from "./pages/NotFound";
+import { ROUTES } from "./routes/Routes";
 
 
 export function App() {
   return <>
     <BrowserRouter>
       <Routes>
-          <Route path="/"  element={<HomePage/>}/>
-          <Route path="/movie"  element={<Movie/>}/>
-          <Route path="/*"  element={<NotFound/>}/>
+          <Route path={ROUTES.HOME}  element={<HomePage/>}/>
+          <Route path={ROUTES.MOVIE}   element={<Movie/>}/>
+          <Route path={ROUTES.NOT_FOUND}   element={<NotFound/>}/>
       </Routes>
     </BrowserRouter>
 </>
